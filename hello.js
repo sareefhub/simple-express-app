a1013b83f8b8eb5436174d1f609b93554389b33c
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2025-09-01 11:29:26 +0700

    fix

--- hello.js
+++ hello.js
@@ -1,8 +1,4 @@
 function sayHello(name) {
-  let unusedVar = 123;        // ❌ unused variable (Code Smell)
-  if (name == "admin") {      // ❌ ใช้ == แทน === (Bug/Code Smell)
-    console.log("Admin login"); // ❌ console.log ใน production code
-  }
   return `Hello, ${name}!`;
 }
 
